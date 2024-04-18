a03be41d45e5aeaa06e00f69380563c2ef688aa9
alexba2004 <j.alejandro.briones.a22@gmail.com>
2024-04-18 07:06:30 -0600

    add: gráficas, iconos y rutas

--- src/controllers/general.controller.js
+++ src/controllers/general.controller.js
@@ -6,97 +6,96 @@ import garajeDao from "../dao/garaje.dao.js";
 import conn from "../database.js";
 import { sensorSchema } from "../models/sensor.model.js";
 
-const generalController = {}
+const generalController = {};
 
 generalController.deleteData = async (req, res) => {
-    try {
-        // Ejecutar ambas funciones al mismo tiempo utilizando Promise.all()
-        const result= await Promise.all([
-            bathroomDao.deleteData(), //2
-            roomDao.deleteData(), // 3
-            kitchenDao.deleteData(), 
-            livingRoomDao.deleteData(),
-            garajeDao.deleteData()
-        ]);
+  try {
+    // Ejecutar ambas funciones al mismo tiempo utilizando Promise.all()
+    const result = await Promise.all([
+      bathroomDao.deleteData(), //2
+      roomDao.deleteData(), // 3
+      kitchenDao.deleteData(),
+      livingRoomDao.deleteData(),
+      garajeDao.deleteData(),
+    ]);
 
-        // Enviar respuesta con los datos de ambas operaciones
-        res.json({ result });
-    } catch (err) {
-        // Manejar errores
-        res.status(500).json({
-            success: false,
-            error: err.msg
-        });
-    }
-}
+    // Enviar respuesta con los datos de ambas operaciones
+    res.json({ result });
+  } catch (err) {
+    // Manejar errores
+    res.status(500).json({
+      success: false,
+      error: err.msg,
+    });
+  }
+};
 
 generalController.getDataChart = async (req, res) => {
-    try {
+  try {
+    // Obtiene la lista de nombres de colecciones
+    const colecciones = await conn.connection.db.listCollections().toArray();
 
-        // Obtiene la lista de nombres de colecciones
-        const colecciones = await conn.connection.db.listCollections().toArray();
+    // Objeto para almacenar los resultados
+    const resultados = {};
 
-        // Objeto para almacenar los resultados
-        const resultados = {};
+    // Iterar sobre cada colección
+    for (const { name } of colecciones) {
+      // Obtener el modelo asociado a la colección actual
+      const Model = conn.model(name, sensorSchema);
 
-        // Iterar sobre cada colección
-        for (const { name } of colecciones) {
-            // Obtener el modelo asociado a la colección actual
-            const Model = conn.model(name, sensorSchema);
+      // Contar los documentos en la colección actual
+      const totalRegistros = await Model.countDocuments({ type: "Actuador" });
 
-            // Contar los documentos en la colección actual
-            const totalRegistros = await Model.countDocuments({type:"Actuador"});
-
-            // Almacenar el total de registros en el objeto de resultados
-            resultados[name] = totalRegistros;
-        }
-
-        // Cerrar la conexión a la base de datos
-    
-
-        // Enviar los resultados como respuesta en formato JSON
-        res.json(resultados);
-    } catch (error) {
-        console.error('Error al obtener el recuento de documentos:', error);
-        res.status(500).json({ error: 'Ocurrió un error al procesar la solicitud' });
+      // Almacenar el total de registros en el objeto de resultados
+      resultados[name] = totalRegistros;
     }
 
-}
+    // Cerrar la conexión a la base de datos
+
+    // Enviar los resultados como respuesta en formato JSON
+    res.json(resultados);
+  } catch (error) {
+    console.error("Error al obtener el recuento de documentos:", error);
+    res
+      .status(500)
+      .json({ error: "Ocurrió un error al procesar la solicitud" });
+  }
+};
 
 generalController.getSensorsAndActions = async (req, res) => {
-    try {
+  try {
+    // Obtiene la lista de nombres de colecciones
+    const colecciones = await conn.connection.db.listCollections().toArray();
 
-        // Obtiene la lista de nombres de colecciones
-        const colecciones = await conn.connection.db.listCollections().toArray();
+    // Objeto para almacenar los resultados
+    const resultados = {};
 
-        // Objeto para almacenar los resultados
-        const resultados = {};
+    // Iterar sobre cada colección
+    for (const { name } of colecciones) {
+      // Obtener el modelo asociado a la colección actual
+      const Model = conn.model(name, sensorSchema);
 
-        // Iterar sobre cada colección
-        for (const { name } of colecciones) {
-            // Obtener el modelo asociado a la colección actual
-            const Model = conn.model(name, sensorSchema);
+      // Contar los documentos en la colección actual
+      const totalSensores = await Model.countDocuments({ type: "Sensor" });
+      const totalActuadores = await Model.countDocuments({ type: "Actuador" });
 
-            // Contar los documentos en la colección actual
-            const totalSensores = await Model.countDocuments({type:"Sensor"});
-            const totalActuadores = await Model.countDocuments({type:"Actuador"});
-
-            // Almacenar el total de registros en el objeto de resultados
-            resultados[name] = {
-                sensores: totalSensores,
-                actuadores: totalActuadores
-            };
-        }
-
-        // Cerrar la conexión a la base de datos
-    
-
-        // Enviar los resultados como respuesta en formato JSON
-        res.json(resultados);
-    } catch (error) {
-        console.error('Error al obtener el recuento de documentos:', error);
-        res.status(500).json({ error: 'Ocurrió un error al procesar la solicitud' });
+      // Almacenar el total de registros en el objeto de resultados
+      resultados[name] = {
+        sensores: totalSensores,
+        actuadores: totalActuadores,
+      };
     }
-}
+
+    // Cerrar la conexión a la base de datos
+
+    // Enviar los resultados como respuesta en formato JSON
+    res.json(resultados);
+  } catch (error) {
+    console.error("Error al obtener el recuento de documentos:", error);
+    res
+      .status(500)
+      .json({ error: "Ocurrió un error al procesar la solicitud" });
+  }
+};
 
 export default generalController;
--- src/routes/routesFront/front.Routes.js
+++ src/routes/routesFront/front.Routes.js
@@ -10,93 +10,89 @@ const router = Router();
 
 //TEMPERATURA
 
-router.get('/room1/temperature', roomController.getOneTemperature)
-router.get('/room2/temperature', roomController.getTwoTemperature)
-router.get('/room3/temperature', roomController.getThreeTemperature)
-router.get('/livingroom/temperature', livingRoomController.getTemperature)
-
+router.get("/room1/temperature", roomController.getOneTemperature);
+router.get("/room2/temperature", roomController.getTwoTemperature);
+router.get("/room3/temperature", roomController.getThreeTemperature);
+router.get("/livingroom/temperature", livingRoomController.getTemperature);
 
 //PRESENCIA
-router.get('/bathroom1/presence', bathroomController.getOnePresence);
-router.get('/bathroom2/presence', bathroomController.getTwoPresence);
-router.get('/garaje/presence', garajeController.getPresence)
+router.get("/bathroom1/presence", bathroomController.getOnePresence);
+router.get("/bathroom2/presence", bathroomController.getTwoPresence);
+router.get("/garaje/presence", garajeController.getPresence);
 
 //LUZ INTERIOR SON 8
 
-router.get('/room1/internal-light', roomController.getInternalLightOne)
-router.get('/room2/internal-light', roomController.getInternalLightTwo)
-router.get('/room3/internal-light', roomController.getInternalLightthree)
-router.get('/bathroom1/internal-light', bathroomController.getInternalLightOne)
-router.get('/bathroom2/internal-light', bathroomController.getInternalLightTwo)
-router.get('/livingroom/internal-light', livingRoomController.getInternalLight)
-router.get('/garaje/internal-light', garajeController.getInternalLight)
+router.get("/room1/internal-light", roomController.getInternalLightOne);
+router.get("/room2/internal-light", roomController.getInternalLightTwo);
+router.get("/room3/internal-light", roomController.getInternalLightthree);
+router.get("/bathroom1/internal-light", bathroomController.getInternalLightOne);
+router.get("/bathroom2/internal-light", bathroomController.getInternalLightTwo);
+router.get("/livingroom/internal-light", livingRoomController.getInternalLight);
+router.get("/garaje/internal-light", garajeController.getInternalLight);
 
 // VENTILADOR
 
-router.get('/room1/fan', roomController.getFanOne)
-router.get('/room2/fan', roomController.getFanTwo)
-router.get('/room3/fan', roomController.getFanThree)
-router.get('/livingroom/fan', livingRoomController.getFan)
+router.get("/room1/fan", roomController.getFanOne);
+router.get("/room2/fan", roomController.getFanTwo);
+router.get("/room3/fan", roomController.getFanThree);
+router.get("/livingroom/fan", livingRoomController.getFan);
 
 // PUERTAS
-router.get('/room1/door', roomController.getDoorOne)
-router.get('/room2/door', roomController.getDoorTwo)
-router.get('/room3/door', roomController.getDoorThree)
-router.get('/livingroom/door', livingRoomController.getDoor)
-router.get('/garaje/door', garajeController.getDoor)
-router.get('/bathroom1/door', bathroomController.getDoorOne)
-router.get('/bathroom2/door', bathroomController.getDoorTwo)
+router.get("/room1/door", roomController.getDoorOne);
+router.get("/room2/door", roomController.getDoorTwo);
+router.get("/room3/door", roomController.getDoorThree);
+router.get("/livingroom/door", livingRoomController.getDoor);
+router.get("/garaje/door", garajeController.getDoor);
+router.get("/bathroom1/door", bathroomController.getDoorOne);
+router.get("/bathroom2/door", bathroomController.getDoorTwo);
 
 // VENTANAS
-router.get('/bathroom1/window', bathroomController.getWindowOne)
-router.get('/bathroom2/window', bathroomController.getWindowTwo)
-router.get('/livingroom/window', livingRoomController.getWindow)
-router.get('/room1/window', roomController.getWindowOne)
-router.get('/room2/window', roomController.getWindowTwo)
-router.get('/room3/window', roomController.getWindowThree)
-router.get('/garaje/window', garajeController.getWindow)
+router.get("/bathroom1/window", bathroomController.getWindowOne);
+router.get("/bathroom2/window", bathroomController.getWindowTwo);
+router.get("/livingroom/window", livingRoomController.getWindow);
+router.get("/room1/window", roomController.getWindowOne);
+router.get("/room2/window", roomController.getWindowTwo);
+router.get("/room3/window", roomController.getWindowThree);
+router.get("/garaje/window", garajeController.getWindow);
 
 // COCINA
-router.get('/kitchen/temperature', kitchenController.getTemperature)
-router.get('/kitchen/gas', kitchenController.getGasConcentration)
-router.get('/kitchen/fan', kitchenController.getFan)
-router.get('/kitchen/window', kitchenController.getWindow)
-router.get('/kitchen/internal-light', kitchenController.getInternalLight)
-router.get('/kitchen/door', kitchenController.getDoor)
-router.get('/kitchen/external-light', kitchenController.getExternalLight)
+router.get("/kitchen/temperature", kitchenController.getTemperature);
+router.get("/kitchen/gas", kitchenController.getGasConcentration);
+router.get("/kitchen/fan", kitchenController.getFan);
+router.get("/kitchen/window", kitchenController.getWindow);
+router.get("/kitchen/internal-light", kitchenController.getInternalLight);
+router.get("/kitchen/door", kitchenController.getDoor);
+router.get("/kitchen/external-light", kitchenController.getExternalLight);
 
 //luz exterior
-router.get('/room1/external-light', roomController.getExternalLightOne)
-router.get('/room2/external-light', roomController.getExternalLightTwo)
-router.get('/room3/external-light', roomController.getExternalLightThree)
-router.get('/garaje/distance', garajeController.getDistance)
-router.get('/garaje/gate', garajeController.getGate)
+router.get("/room1/external-light", roomController.getExternalLightOne);
+router.get("/room2/external-light", roomController.getExternalLightTwo);
+router.get("/room3/external-light", roomController.getExternalLightThree);
+router.get("/garaje/distance", garajeController.getDistance);
+router.get("/garaje/gate", garajeController.getGate);
 
 //TODOS LOS DATOS
-router.get('/kitchen', kitchenController.getAll)
-router.get('/bathroom1', bathroomController.getAllOne)
-router.get('/bathroom2', bathroomController.getAllTwo)
-router.get('/room1', roomController.getAllOne)
-router.get('/room2', roomController.getAllTwo)
-router.get('/room3', roomController.getAllThree)
-router.get('/livingroom', livingRoomController.getAll)
-router.get('/garaje', garajeController.getAll)
-
+router.get("/kitchen", kitchenController.getAll);
+router.get("/bathroom1", bathroomController.getAllOne);
+router.get("/bathroom2", bathroomController.getAllTwo);
+router.get("/room1", roomController.getAllOne);
+router.get("/room2", roomController.getAllTwo);
+router.get("/room3", roomController.getAllThree);
+router.get("/livingroom", livingRoomController.getAll);
+router.get("/garaje", garajeController.getAll);
 
 // GRAFICA INDEPENDIENTES
-router.get('/chart/kitchen', kitchenController.getDataChart)
-router.get('/chart/livingroom', livingRoomController.getDataChart)
+router.get("/chart/kitchen", kitchenController.getDataChart);
+router.get("/chart/livingroom", livingRoomController.getDataChart);
 
 //GRAFICA PROMEDIOS
-router.get('/kitchen/avg', kitchenController.getAverage)
-router.get('/livingroom/avg', livingRoomController.getAverage)
-router.get('/room1/avg', roomController.getAvg1)
-router.get('/room2/avg', roomController.getAvg2)
-router.get('/room3/avg', roomController.getAvg3)
-
+router.get("/kitchen/avg", kitchenController.getAverage);
+router.get("/livingroom/avg", livingRoomController.getAverage);
+router.get("/room1/avg", roomController.getAvg1);
+router.get("/room2/avg", roomController.getAvg2);
+router.get("/room3/avg", roomController.getAvg3);
 
 //GRAFICA GENERAL
-router.get('/chart', generalController.getDataChart)
-router.get('/chart/v2', generalController.getSensorsAndActions)
+router.get("/chart", generalController.getDataChart);
+router.get("/chart/v2", generalController.getSensorsAndActions);
 export default router;
-
